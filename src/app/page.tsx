f196be4c8ca332b9017e0411aa188fc1b1c032a8
Amama-Fatima <amamafatima58@gmail.com>
2026-04-17 00:18:18 +0500

    resume link updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
                 text={DATA.description}
               />
               <Link
-                href="https://drive.google.com/file/d/1NaFj2jWvGYFbMU9RZl7Mrs5VfvApr5tA/view?usp=sharing"
+                href="https://drive.google.com/file/d/15HjVO2N8S-jvaXMLDYrbOMnj0puOmFyd/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-600 underline hover:text-blue-800 font-medium mt-2 w-fit"
